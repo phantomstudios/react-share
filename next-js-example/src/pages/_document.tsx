2c9d4ffe83659d662b4dfa086919c59a790fdf9a
Balraj Johal <balraj.johal@phntms.com>
2023-04-14 16:26:56 +0100

    TEst

--- next-js-example/src/pages/_document.tsx
+++ next-js-example/src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import PageLayout from "@/components/PageLayout";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+      <PageLayout>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </PageLayout>
     </Html>
-  )
+  );
 }
--- next-js-example/src/pages/subroute/index.tsx
+++ next-js-example/src/pages/subroute/index.tsx
@@ -1,16 +1,12 @@
 import { Inter } from "next/font/google";
-import UtilExamples from "@/components/UtilExamples";
 
 const inter = Inter({ subsets: ["latin"] });
-
-const exampleURL = "https://www.npmjs.com/package/@phntms/react-share";
 
 export default function Home() {
   return (
     <>
       <main className={inter.className}>
-        <h1>React Share Demo</h1>
-        <UtilExamples url={exampleURL} />
+        <h1>Subroute Example</h1>
       </main>
     </>
   );
